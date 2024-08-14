790e9b1fc6c208e77a3899435ff5d8729a4dfe27
vsaldivarm <vladimir.saldivar@morsco.com>
2024-08-14 13:50:52 -0600

    data class created

---
+++ shared/src/commonMain/kotlin/com/vsaldivarm/dailypulse/articles/Article.kt
@@ -0,0 +1,8 @@
+package com.vsaldivarm.dailypulse.articles
+
+data class Article(
+    val title: String,
+    val desc: String,
+    val date: String,
+    val imageUrl: String
+)
--- shared/src/commonMain/kotlin/com/vsaldivarm/dailypulse/articles/ArticlesState.kt
+++ shared/src/commonMain/kotlin/com/vsaldivarm/dailypulse/articles/ArticlesState.kt
@@ -1,5 +1,7 @@
 package com.vsaldivarm.dailypulse.articles
 
-class ArticlesState {
-
-}
+data class ArticlesState(
+    val articles: List<Article> = listOf(),
+    val loading: Boolean = false,
+    val error: String? = null
+)
